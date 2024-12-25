2102102d210d2fc1427de0789576358f2e9c931c
Daniel Gafni <danielgafni16@gmail.com>
2024-12-25 18:54:29 +0200

    [dagster-pipes-java] add tests in CI (#77)
    Run Dagster integration tests in CI for Java Pipes

--- libraries/pipes/implementations/java/dagster-pipes-java/nix/shells/default/default.nix
+++ libraries/pipes/implementations/java/dagster-pipes-java/nix/shells/default/default.nix
@@ -1,16 +1,22 @@
 {
+  lib,
+  stdenv,
   pkgs,
   mkShell,
   ...
 }:
+let python = pkgs.python310; in
 mkShell {
   # Create your shell
   packages = with pkgs; [
     zsh
     jdk8
     uv
+    python
   ];
   shellHook = ''
-    exec zsh
+    export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [stdenv.cc.cc]}
+    export UV_PYTHON=${python}/bin/python
   '';
 }
