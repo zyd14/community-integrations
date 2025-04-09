3fd7eabff8f70f97d3ea1dd529ee19a717246ed0
Colton Padden <colton@dagsterlabs.com>
2025-04-09 14:53:57 -0400

    [dagster-evidence] register EvidenceProject to __all__

--- libraries/dagster-evidence/dagster_evidence/__init__.py
+++ libraries/dagster-evidence/dagster_evidence/__init__.py
@@ -1,5 +1,9 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
+from dagster_evidence.lib.evidence_project import EvidenceProject
+
 __version__ = "0.1.0"
+
+__all__ = [EvidenceProject]
 
 DagsterLibraryRegistry.register("dagster-evidence", __version__)
