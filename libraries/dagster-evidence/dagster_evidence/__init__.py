15ee371489a38c27ebbfb7dd09f38b1b9c145f9c
Colton Padden <colton@dagsterlabs.com>
2025-04-09 15:02:27 -0400

    Release dagster-evidence 0.1.2

--- libraries/dagster-evidence/dagster_evidence/__init__.py
+++ libraries/dagster-evidence/dagster_evidence/__init__.py
@@ -2,7 +2,7 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_evidence.lib.evidence_project import EvidenceProject
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [EvidenceProject]
 
