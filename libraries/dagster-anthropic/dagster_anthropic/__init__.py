84007f597168483d1d78e936808cef01b6bd3a04
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-09 18:21:53 -0600

    Release dagster-anthropic 0.0.3

--- libraries/dagster-anthropic/dagster_anthropic/__init__.py
+++ libraries/dagster-anthropic/dagster_anthropic/__init__.py
@@ -2,6 +2,6 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_anthropic.resource import AnthropicResource as AnthropicResource
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 DagsterLibraryRegistry.register("dagster-anthropic", __version__)
