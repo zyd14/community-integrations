cc9d670c505203616787b879ed3c015d47f3a8bb
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-09 18:37:06 -0600

    Release dagster-gemini 0.0.3

--- libraries/dagster-gemini/dagster_gemini/__init__.py
+++ libraries/dagster-gemini/dagster_gemini/__init__.py
@@ -2,6 +2,6 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_gemini.resource import GeminiResource as GeminiResource
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 DagsterLibraryRegistry.register("dagster-gemini", __version__)
