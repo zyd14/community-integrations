f381e382ea4d0364460b5fa2092a59d60db1eb92
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-09 18:39:08 -0600

    Release dagster-modal 0.0.3

--- libraries/dagster-modal/dagster_modal/__init__.py
+++ libraries/dagster-modal/dagster_modal/__init__.py
@@ -2,6 +2,6 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_modal.resources import ModalClient as ModalClient
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 DagsterLibraryRegistry.register("dagster-modal", __version__)
