213b0c763dc54ac07c5736792ce9c3c5954824b6
Colton Padden <colton@dagsterlabs.com>
2025-09-02 09:57:38 -0400

    Release dagster-dataform 0.0.4

--- libraries/dagster-dataform/dagster_dataform/__init__.py
+++ libraries/dagster-dataform/dagster_dataform/__init__.py
@@ -10,7 +10,7 @@ from dagster_dataform.dataform_orchestration_schedule import (
     create_dataform_orchestration_schedule as create_dataform_orchestration_schedule,
 )
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 DagsterLibraryRegistry.register(
     "dagster-dataform", __version__, is_dagster_package=False
