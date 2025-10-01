e74759d9c26087ea28ed92cc23179f19584fc117
zyd14 <romerzs14@gmail.com>
2025-10-01 10:00:15 -0700

    ruff

--- libraries/dagster-iceberg/tests/io_managers/test_polars_io_manager.py
+++ libraries/dagster-iceberg/tests/io_managers/test_polars_io_manager.py
@@ -102,9 +102,7 @@ def daily_partitioned(context: AssetExecutionContext) -> pl.DataFrame:
     key_prefix=["my_schema"],
     partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"),
     config_schema={"value": str},
-    metadata={"partition_expr": "partition",
-              "write_mode": "overwrite"
-              },
+    metadata={"partition_expr": "partition", "write_mode": "overwrite"},
 )
 def daily_partitioned_append_mode(context: AssetExecutionContext) -> pl.DataFrame:
     partition = datetime.datetime.strptime(context.partition_key, "%Y-%m-%d").date()
