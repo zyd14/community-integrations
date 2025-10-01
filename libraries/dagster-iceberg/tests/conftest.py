1e6d52ceddf13f95fdc9187990f9f2708e4648d4
zyd14 <romerzs14@gmail.com>
2025-10-01 10:00:30 -0700

    use if_not_exists instead of suppress

--- libraries/dagster-iceberg/tests/conftest.py
+++ libraries/dagster-iceberg/tests/conftest.py
@@ -119,10 +119,7 @@ def namespace_name() -> str:
 
 @pytest.fixture(autouse=True)
 def namespace(catalog: Catalog, namespace_name: str) -> str:
-    from sqlalchemy.exc import IntegrityError
-
-    with contextlib.suppress(IntegrityError):
-        catalog.create_namespace(namespace_name)
+    catalog.create_namespace_if_not_exists(namespace_name)
     return namespace_name
 
 
