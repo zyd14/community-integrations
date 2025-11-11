0b59d719329239ff05689ee3a400f128006066d3
zyd14 <romerzs14@gmail.com>
2025-11-10 18:51:10 -0800

    ruff

--- libraries/dagster-iceberg/tests/conftest.py
+++ libraries/dagster-iceberg/tests/conftest.py
@@ -1,12 +1,11 @@
 import datetime as dt
+import logging
 import os
 import random
 import shutil
 import subprocess
 from collections.abc import Iterator
 from time import sleep
-import logging
-
 
 import psycopg2
 import pyarrow as pa
@@ -16,7 +15,7 @@ from dotenv import load_dotenv
 from pyiceberg.catalog import Catalog, load_catalog
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 COMPOSE_DIR = file_relative_path(__file__, "docker")
@@ -48,9 +47,9 @@ def _compose(tmp_path_factory: pytest.TempPathFactory) -> Iterator[None]:
             env={"WAREHOUSE_PATH": warehouse_path, **os.environ},
         )
     except subprocess.CalledProcessError as e:
-        logger.error(f"Docker compose up failed with return code {e.returncode}")
-        logger.error(f"STDOUT:\n{e.stdout}")
-        logger.error(f"STDERR:\n{e.stderr}")
+        logger.error("Docker compose up failed with return code %s", e.returncode)
+        logger.error("STDOUT:\n%s", e.stdout)
+        logger.error("STDERR:\n%s", e.stderr)
         raise
     sleep(5)
     yield
