877c4175e7f2923d77b505085e59ea2064c30ea9
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:56:59 -0500

    Release dagster-hightouch 0.1.7

--- libraries/dagster-hightouch/dagster_hightouch/__init__.py
+++ libraries/dagster-hightouch/dagster_hightouch/__init__.py
@@ -1,6 +1,6 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 DagsterLibraryRegistry.register(
     "dagster-hightouch", __version__, is_dagster_package=False
