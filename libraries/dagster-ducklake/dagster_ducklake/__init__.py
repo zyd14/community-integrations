c1017835074fcfae63b49551d6dec024bdb1ddf1
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:56:34 -0500

    Release dagster-ducklake 0.0.2

--- libraries/dagster-ducklake/dagster_ducklake/__init__.py
+++ libraries/dagster-ducklake/dagster_ducklake/__init__.py
@@ -22,6 +22,6 @@ __all__ = [
     "DuckDBConnectionProvider",
 ]
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 DagsterLibraryRegistry.register("dagster-duckdb", __version__, is_dagster_package=False)
