b97a115c737dcf313c27ce02246af87c832f260c
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:55:31 -0500

    Release dagster-chroma 0.0.3

--- libraries/dagster-chroma/dagster_chroma/__init__.py
+++ libraries/dagster-chroma/dagster_chroma/__init__.py
@@ -9,6 +9,6 @@ from dagster_chroma.config import (
     HttpConfig as HttpConfig,
 )
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 DagsterLibraryRegistry.register("dagster-chroma", __version__, is_dagster_package=False)
