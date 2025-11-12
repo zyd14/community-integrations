69ec37721a3403c57dbe2c820b23964e02ad925e
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:56:50 -0500

    Release dagster-gemini 0.0.5

--- libraries/dagster-gemini/dagster_gemini/__init__.py
+++ libraries/dagster-gemini/dagster_gemini/__init__.py
@@ -2,6 +2,6 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_gemini.resource import GeminiResource as GeminiResource
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 DagsterLibraryRegistry.register("dagster-gemini", __version__, is_dagster_package=False)
