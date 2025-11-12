5dec1d4f9f6f427f9e6ddfebc84056ae6afd9b86
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:57:16 -0500

    Release dagster-qdrant 0.0.3

--- libraries/dagster-qdrant/dagster_qdrant/__init__.py
+++ libraries/dagster-qdrant/dagster_qdrant/__init__.py
@@ -4,6 +4,6 @@ from dagster_qdrant.config import QdrantConfig
 from dagster_qdrant.resource import QdrantResource
 
 __all__ = ["QdrantConfig", "QdrantResource"]
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 DagsterLibraryRegistry.register("dagster-qdrant", __version__, is_dagster_package=False)
