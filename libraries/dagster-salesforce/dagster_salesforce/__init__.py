17a885f5ae662e738f2585ed0f4bff54aa3cdcbf
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:57:19 -0500

    Release dagster-salesforce 0.0.2

--- libraries/dagster-salesforce/dagster_salesforce/__init__.py
+++ libraries/dagster-salesforce/dagster_salesforce/__init__.py
@@ -8,7 +8,7 @@ from dagster_salesforce.resource import (
     SalesforceField as SalesforceField,
 )
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 DagsterLibraryRegistry.register(
     "dagster-salesforce", __version__, is_dagster_package=False
