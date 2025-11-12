9f8fbe22a48cefbc264879a8000fcdb094dff372
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:57:05 -0500

    Release dagster-modal 0.0.4

--- libraries/dagster-modal/dagster_modal/__init__.py
+++ libraries/dagster-modal/dagster_modal/__init__.py
@@ -2,6 +2,6 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_modal.resources import ModalClient as ModalClient
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 DagsterLibraryRegistry.register("dagster-modal", __version__, is_dagster_package=False)
