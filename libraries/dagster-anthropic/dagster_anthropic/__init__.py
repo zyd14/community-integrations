aa9c730c372fab9e1306a5c5ce73614bc91d66ea
Colton Padden <colton@dagsterlabs.com>
2025-11-12 10:54:21 -0500

    Release dagster-anthropic 0.0.5

--- libraries/dagster-anthropic/dagster_anthropic/__init__.py
+++ libraries/dagster-anthropic/dagster_anthropic/__init__.py
@@ -2,7 +2,7 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_anthropic.resource import AnthropicResource as AnthropicResource
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 DagsterLibraryRegistry.register(
     "dagster-anthropic", __version__, is_dagster_package=False
