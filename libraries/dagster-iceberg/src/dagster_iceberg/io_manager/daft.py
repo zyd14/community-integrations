0be1ac56b8794b4141ea80478ee44b03bbdbacdc
zyd14 <romerzs14@gmail.com>
2025-11-14 13:30:45 -0800

    TableScan objects don't have a `to_daft` function, so we need to convert from arrow

--- libraries/dagster-iceberg/src/dagster_iceberg/io_manager/daft.py
+++ libraries/dagster-iceberg/src/dagster_iceberg/io_manager/daft.py
@@ -37,13 +37,9 @@ class _DaftIcebergTypeHandler(_handler.IcebergBaseTypeHandler[da.DataFrame]):
             ).partition_dimensions_to_filters()
             row_filter = " AND ".join(expressions)
 
-        ddf = table.scan(snapshot_id=snapshot_id).to_daft()  # noqa: F841, `daft.sql` detects `daft.DataFrame` objects
+        ddf: da.DataFrame = da.from_arrow(table.scan(snapshot_id=snapshot_id).to_arrow())
 
-        stmt = f"SELECT {selected_fields} FROM ddf"
-        if row_filter is not None:
-            stmt += f"\nWHERE {row_filter}"
-
-        return da.sql(stmt)
+        return ddf.select(selected_fields).where(row_filter) if row_filter is not None else ddf.select(selected_fields)
 
     def to_arrow(self, obj: da.DataFrame) -> pa.Table:
         return obj.to_arrow()
