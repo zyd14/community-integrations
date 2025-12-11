0e536860488e7405839c9c18eabd56609d0d5d03
Colton Padden <colton@dagsterlabs.com>
2025-12-11 13:51:05 -0500

    Release dagster-contrib-gcp 0.0.9

--- libraries/dagster-contrib-gcp/dagster_contrib_gcp/__init__.py
+++ libraries/dagster-contrib-gcp/dagster_contrib_gcp/__init__.py
@@ -1,6 +1,6 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-__version__ = "0.0.5"
+__version__ = "0.0.9"
 
 DagsterLibraryRegistry.register(
     "dagster-contrib-gcp", __version__, is_dagster_package=False
